c37258cd21428c1ecd93f89e911da2dedeaaf04f
Artyr <artyrdogtar@gmail.com>
2022-11-30 23:25:33 +0200

    home work 2 done

---
+++ src/Snail.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Snail {
+
+    public static void main(String[] args)throws Exception {
+        Scanner scan = new Scanner(System.in);
+        int speedUp = scan.nextInt();
+        int speedDown = scan.nextInt();
+        int height = scan.nextInt();
+        if (height<=speedUp) System.out.println("1");
+        else if (speedUp<=speedDown) System.out.println("Impossible");
+        else System.out.println((height - speedUp) / (speedUp - speedDown) + 1 );
+
+    }
+
+}
