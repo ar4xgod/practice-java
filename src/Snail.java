2c66c102e7311886839ed0f423e085ad5fab4837
Artyr <artyrdogtar@gmail.com>
2022-11-30 23:50:18 +0200

    removed unused code

--- src/Snail.java
+++ src/Snail.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 public class Snail {
 
-    public static void main(String[] args)throws Exception {
+    public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         int speedUp = scan.nextInt();
         int speedDown = scan.nextInt();
         int height = scan.nextInt();
         if (height<=speedUp) System.out.println("1");
         else if (speedUp<=speedDown) System.out.println("Impossible");
-        else System.out.println((height - speedUp) / (speedUp - speedDown) + 1 );
+        else System.out.println((height - speedUp) / (speedUp - speedDown) +1);
 
     }
 
